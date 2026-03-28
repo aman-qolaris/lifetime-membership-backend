1d211b71f1d172ef5b9a08e489e0ad32cf48f39f
Sakshi-1224 <sakshi12bpl@gmail.com>
2026-03-28 23:30:04 +0530

    changes by me sakshi

--- membership-backend/src/modules/admins/dtos/admin.actions.dto.js
+++ membership-backend/src/modules/admins/dtos/admin.actions.dto.js
@@ -23,7 +23,38 @@ export const updateFeeDto = Joi.object({
   amount: Joi.number().positive().required(),
 });
 
-export const editApplicantDto = Joi.object().min(1).unknown(true);
+// Replace your old editApplicantDto with this!
+export const editApplicantDto = Joi.object({
+  fullName: Joi.string(),
+  fatherOrHusbandName: Joi.string(),
+  gender: Joi.string(),
+  dateOfBirth: Joi.date(),
+  marriageDate: Joi.date().allow(null, ""),
+  bloodGroup: Joi.string().allow(null, ""),
+  education: Joi.string(),
+  occupation: Joi.string(),
+  mobileNumber: Joi.string(),
+  email: Joi.string().email(),
+  currentAddress: Joi.string(),
+  permanentAddress: Joi.string(),
+  officeAddress: Joi.string().allow(null, ""),
+  isFromRaipur: Joi.boolean(),
+  region: Joi.string().allow(null, ""),
+  membershipType: Joi.string()
+})
+  .rename("full_name", "fullName", { override: true, ignoreUndefined: true })
+  .rename("father_or_husband_name", "fatherOrHusbandName", { override: true, ignoreUndefined: true })
+  .rename("date_of_birth", "dateOfBirth", { override: true, ignoreUndefined: true })
+  .rename("marriage_date", "marriageDate", { override: true, ignoreUndefined: true })
+  .rename("blood_group", "bloodGroup", { override: true, ignoreUndefined: true })
+  .rename("mobile_number", "mobileNumber", { override: true, ignoreUndefined: true })
+  .rename("current_address", "currentAddress", { override: true, ignoreUndefined: true })
+  .rename("permanent_address", "permanentAddress", { override: true, ignoreUndefined: true })
+  .rename("office_address", "officeAddress", { override: true, ignoreUndefined: true })
+  .rename("is_from_raipur", "isFromRaipur", { override: true, ignoreUndefined: true })
+  .rename("membership_type", "membershipType", { override: true, ignoreUndefined: true })
+  .min(1)
+  .unknown(true);
 
 export const idParamsDto = Joi.object({
   id: Joi.string().uuid({ version: "uuidv4" }).required(),
